6075462c216f3e94d507da8b9af63ce4cff2bbec
gyanisgod <gyanhigashi@gmail.com>
2017-12-19 14:46:21 +0900

    Excel修正とシーン変更

--- Assets/Dobashi/Script/CommandSkill.cs
+++ Assets/Dobashi/Script/CommandSkill.cs
@@ -7,7 +7,9 @@ public enum Command_Skill_List
     Doubleattack,        //2連撃
     Berserk,             //次の攻撃時必殺＋15%命中-15%
     Destruction,         //次の攻撃時攻撃+10守備-10
-    Stability            //お互いに追撃不可
+    Stability,           //お互いに追撃不可
+    Emblemburst,         //HP-5して3ターンの間力に呪力を追加
+    Emblemawakening      //Hp-10して5ターンの間、命中と回避と必殺を(10 + Lv)プラスする
 }
 
 public class CommandSkill : MonoBehaviour {
@@ -50,6 +52,16 @@ public class CommandSkill : MonoBehaviour {
             case Command_Skill_List.Stability:
                 Debug.Log("次の攻撃時お互いに追撃不可");
                 _chara.GetComponent<Character>()._stability = true;
+                break;
+            case Command_Skill_List.Emblemburst:
+                Debug.Log("3ターンの間、力に呪力を追加");
+                _chara.GetComponent<Character>()._totalhp -= 5;
+                _chara.GetComponent<Character>().AddBuff(_chara.GetComponent<Character>()._totalcur,0,0,0,0,0,0,0,0,0,0,0,0,3);
+                break;
+            case Command_Skill_List.Emblemawakening:
+                Debug.Log("5ターンの間、命中と回避と必殺を(10 + Lv)プラスする");
+                _chara.GetComponent<Character>()._totalhp -= 10;
+                _chara.GetComponent<Character>().AddBuff(0,0,0,0,0,0,0,10 + _chara.GetComponent<Character>()._totalLevel, 10 + _chara.GetComponent<Character>()._totalLevel, 10 + _chara.GetComponent<Character>()._totalLevel, 0,0,0,5);
                 break;
 
         }
