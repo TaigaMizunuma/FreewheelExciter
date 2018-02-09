574f4ea7d2d5ed5ff0a15f362a11755d44575f40
konge <saiyaku04ana@gmail.com>
2018-02-09 12:06:33 +0900

    RushEnemy改修

--- Assets/Anakubo/Script/RushEnemy.cs
+++ Assets/Anakubo/Script/RushEnemy.cs
@@ -21,29 +21,63 @@ public class RushEnemy : MonoBehaviour {
     public GameObject SetTarget()
     {
         players_ = GameObject.FindGameObjectsWithTag("Player");
-        target_ = null;
-        foreach (GameObject p in players_)
+        List<GameObject> p_ = new List<GameObject>();
+        for(int i = 0; i < players_.Length; i++)
         {
-            GameObject p_pos = p.GetComponent<Move_System>().GetNowPos();
-            if (target_ == null || target_.GetComponent<Square_Info>().GetRushCost() < p_pos.GetComponent<Square_Info>().GetRushCost())
-            {
-                target_ = p_pos;
-            }
+            p_.Add(players_[i]);
         }
-        GameObject target_pos = null;
-        foreach (GameObject t in target_.GetComponent<Square_Info>().GetNear())
+        for(int i = 0; i < p_.Count; i++)
         {
-            if (t.GetComponent<Square_Info>().GetChara() != null) continue;
-            if (target_pos == null) target_pos = t;
-            else if (t.GetComponent<Square_Info>().GetChara() == null)
+            for(int j = 0; j < p_.Count - 1; j++)
             {
-                if (target_pos.GetComponent<Square_Info>().GetMaxCost() < t.GetComponent<Square_Info>().GetMaxCost())
+                if(p_[j].GetComponent<Square_Info>().GetRushCost() > p_[j + 1].GetComponent<Square_Info>().GetRushCost())
                 {
-                    target_pos = t;
+                    GameObject obj = p_[j];
+                    p_[j] = p_[j + 1];
+                    p_[j + 1] = obj;
                 }
             }
         }
+        //target_ = null;
+        //foreach (GameObject p in players_)
+        //{
+        //    GameObject p_pos = p.GetComponent<Move_System>().GetNowPos();
+        //    if (target_ == null || target_.GetComponent<Square_Info>().GetRushCost() < p_pos.GetComponent<Square_Info>().GetRushCost())
+        //    {
+        //        target_ = p_pos;
+        //    }
+        //}
+        
+        GameObject target_pos = null;
+        //foreach (GameObject t in target_.GetComponent<Square_Info>().GetNear())
+        //{
+        //    if (t.GetComponent<Square_Info>().GetChara() != null || t.GetComponent<Square_Info>().GetCost()>GetComponent<EnemyBase>().first_cost) continue;
+        //    if (target_pos == null) target_pos = t;
+        //    else if (t.GetComponent<Square_Info>().GetChara() == null)
+        //    {
+        //        if (target_pos.GetComponent<Square_Info>().GetMaxCost() < t.GetComponent<Square_Info>().GetMaxCost())
+        //        {
+        //            target_pos = t;
+        //        }
+        //    }
+        //}
+        for(int i = 0; i < p_.Count; i++)
+        {
+            foreach(GameObject t in p_[i].GetComponent<Square_Info>().GetNear())
+            {
+                if (t.GetComponent<Square_Info>().GetChara() != null || t.GetComponent<Square_Info>().GetCost() > GetComponent<EnemyBase>().first_cost) continue;
+                if (target_pos == null) target_pos = t;
+                else
+                {
+                    if (target_pos.GetComponent<Square_Info>().GetMaxCost() < t.GetComponent<Square_Info>().GetMaxCost())
+                    {
+                        target_pos = t;
+                    }
+                }
+            }
+            if (target_pos != null) break;
+        }
         players_ = null;
-        return target_;
+        return target_pos;
     }
 }
