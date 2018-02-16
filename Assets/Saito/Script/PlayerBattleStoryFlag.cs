555102808ae4320902d70b943f6e6f0aaaa2f855
Saito <@Saito.com>
2018-02-16 12:25:09 +0900

    PlayerBattleStoryスクリプト追加

---
+++ Assets/Saito/Script/PlayerBattleStoryFlag.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBattleStoryFlag : MonoBehaviour {
+
+    //ストーリーに関わる敵か
+    bool storyEnemy;
+
+    //接敵したか
+    bool nearEnemy;
+
+    //自分(敵キャラ)の名前
+    string playerName;
+
+    //接敵した敵の名前
+    string enemyName;
+
+    void Start()
+    {
+        playerName = this.gameObject.transform.name;
+        storyEnemy = this.GetComponent<EnemyPersonalCSV>().GetStoryFlag();
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 接敵時バトル中の会話フラグ管理クラスに情報を送る
+    /// </summary>
+    void SendStoryFlag()
+    {
+        if (storyEnemy == true && nearEnemy == true)
+        {
+            FindObjectOfType<StoryFlag>().SetCharacterName(playerName, playerName);
+        }
+    }
+
+    void PlayerStoryPlay()
+    {
+
+    }
+
+    /// <summary>
+    /// ボスの名前を入れる
+    /// </summary>
+    /// <param name="enemy"></param>
+    public void SetEnemyName(string enemy)
+    {
+        enemyName = enemy;
+    }
+}
