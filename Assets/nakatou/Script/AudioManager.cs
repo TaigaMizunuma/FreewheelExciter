3acfda82f5c439c95d7d0cae83199e4511ab7206
nakatou <naoya.nakatou-708-1994@docomo.ne.jp>
2018-02-23 13:49:13 +0900

    BGM・SEの音量調整

--- Assets/nakatou/Script/AudioManager.cs
+++ Assets/nakatou/Script/AudioManager.cs
@@ -17,6 +17,9 @@ public class AudioManager : MonoBehaviour
     private Dictionary<string, int> seIndexes = new Dictionary<string, int>();
     private Dictionary<string, int> bgmIndexes = new Dictionary<string, int>();
 
+    public float bgmvolume =0;
+    public float sevolume =1;
+
     void Start()
     {
         seClips = Resources.LoadAll<AudioClip>("SE");
@@ -24,8 +27,10 @@ public class AudioManager : MonoBehaviour
 
         bgmSource = gameObject.AddComponent<AudioSource>();
         bgmSource.loop = true;
+        
         seSource = gameObject.AddComponent<AudioSource>();
         seSource.loop = false;
+       
 
         for (int i = 0; i < seClips.Length; ++i)
         {
@@ -36,12 +41,47 @@ public class AudioManager : MonoBehaviour
         {
             bgmIndexes[bgmClips[i].name] = i;
         }
+
+        //PlayBgm("battle1");
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        bgmSource.volume = bgmvolume;
+        seSource.volume = sevolume;
+
+        //デバッグ用
+        //if (Input.GetKeyDown(KeyCode.RightAlt))
+        //{
+        //    var num = bgmvolume;
+        //    num += 0.1f;
+        //    SetBGMVolum(num);
+        //}
+        //if (Input.GetKeyDown(KeyCode.LeftAlt))
+        //{
+        //    var num = bgmvolume;
+        //    num -= 0.1f;
+        //    SetBGMVolum(num);
+        //}
+    }
+
+    /// <summary>
+    /// BGMの音量調整(0～1)
+    /// </summary>
+    /// <param name="value">0が音なし、1が最大</param>
+    public void SetBGMVolum(float value)
+    {
+        bgmvolume = value;
+    }
+
+    /// <summary>
+    /// SEの音量調整(0～1)
+    /// </summary>
+    /// <param name="value">0が音なし、1が最大</param>
+    public void SetSeVolum(float value)
+    {
+        sevolume = value;
     }
 
     public int GetSeIndex(string name)
